c8c2cb7485ff7f5411b46f09f8081b2138be8289
刘祺 <gucong@gmail.com>
2013-10-09 21:18:40 +0800

    plugins\jQuery.jmvalidate\js\H5F.js plugins\jQuery.jmvalidate\js\jquery.jmvalidate.js plugins\jQuery.scrollLoading\js\jquery.scrollLoading.js plugins\jQuery.selectui\js\jquery.selectui.js

---
+++ plugins/jQuery.scrollLoading/js/jquery.scrollLoading.js
@@ -0,0 +1,157 @@
+/*!
+ * jquery.scrollLoading.js
+ * 图片/非图片滚动加载、懒加载，图片地址异常处理
+ * 二次调用会直接加载图片和触发回调
+ * by zhangxinxu  http://www.zhangxinxu.com
+ * 2010-11-19 v1.0
+ * 2012-01-13 v1.1 偏移值计算修改 position → offset
+ * 2012-09-25 v1.2 增加滚动容器参数, 回调参数
+ * 2013-05-10	对象或图片按需动态加载，为图片提供error时回调接口
+ * @param {Object} options
+ * options.attr 
+ * options.container {Object} 滚动条所在容器Element或jQuery对象或选择器，默认window
+ * options.callback Element滚入视界时的回调，回调中this为Element
+ * options.error 图片地址错误时回调，回调中this为Image
+ * @return {Object} jQuery对象
+ */
+(function($) {
+	//寄存错误的URL
+	var urlErrs = {};
+
+	//获取元素所在页面位置
+	var getOffset = function(o){
+		o = o.closest(":visible");
+		if(o.length){
+			return $.extend(o.offset(), {h: o.height()});
+		}
+	};
+
+	//参数默认值声明
+	var defaults = {
+		attr: "data-url",
+		container: $(window),
+		callback: $.noop,
+		error: function(){
+			//onImgError为整站全局声明的图片异常处理
+			onImgError(this);
+		}
+	};
+
+	$.fn.scrollLoading = function(options) {
+		var params = $.extend({}, defaults, options || {});
+		params.cache = [];
+
+		//触发回调函数
+		var callback = function(call) {
+			if ($.isFunction(params.callback)) {
+				params.callback.call(call.get(0));
+			}
+		};
+
+		//触发图片URL错误回调并记录URL
+		var callerror = function(img, url) {
+			if ($.isFunction(params.error)) {
+				params.error.call(img.get(0));
+			}
+			if(url){
+				urlErrs[url] = true;
+			}
+		};
+	
+		//检查图片url正确与否
+		var waitImg = function(img, url){
+			if(!url){
+				url = img.attr("src");
+				if(img.get(0).complete){
+					var newimg = new Image();
+					newimg.src = url;
+					if(!newimg.width){
+						callerror(img, url);
+					}
+					return;
+				}
+			}
+			$(new Image()).error(function(){
+				callerror(img, url);
+			}).attr("src", url);
+		};
+		
+		//动态显示数据
+		var load = function(cache) {
+			//查找视界位置
+			var contHeight = params.container.height();
+			if ($(window).get(0) === window) {
+				contop = $(window).scrollTop();
+			} else {
+				contop = params.container.offset().top;
+			}
+
+			
+			//检查元素是否在视界内
+			$.each(cache, function(i, data) {
+				var o = data.obj, tag = data.tag, url, post, posb, offset;
+				if (o) {
+					url = o.attr(params["attr"]);
+					if(urlErrs[url]){
+						callerror(o);
+					} else if(offset = getOffset(o)){
+						post = offset.top - contop, posb = post + offset.h;
+						if ((post >= 0 && post < contHeight) || (posb > 0 && posb <= contHeight)) {
+							//在浏览器窗口内
+							data.obj = null;
+							if (tag === "img") {
+								if (url) {
+									//图片，改变src
+									o.attr("src", url);
+									waitImg(o, url);
+								} else if(o.attr("src")){
+									waitImg(o);
+								}
+							}else{
+								if (url) {
+									o.load(url, {}, function() {
+										callback(o);
+									});
+									return;
+								}
+							}
+							// 触发回调
+							callback(o);
+						}
+					}
+				}
+			});
+		};
+		
+		var loading = function(){
+			load(params.cache);
+		};
+
+		this.each(function() {
+			var node = this.nodeName.toLowerCase();
+			var me = $(this);
+			
+			//重组
+			var data = {
+				obj: me,
+				tag: node
+			};
+			var loadFn = me.data("scrollLoading");
+			if(loadFn){
+				loadFn([data]);
+			} else {
+				me.data("scrollLoading", load);
+				params.cache.push(data);
+			}
+		});
+
+		//事件触发
+		//加载完毕即执行
+		if(params.cache.length){
+			$(loading);
+			//滚动执行
+			params.container.bind("scroll resize", loading);
+		}
+		return this;
+	};
+})(jQuery);
---
+++ plugins/jQuery.selectui/js/jquery.selectui.js
@@ -0,0 +1,194 @@
+(function($){
+	var msie = (function(w,d){return ("XMLHttpRequest" in w ? d.querySelector ? d.documentMode : 7 : 6)})(this,this.document),
+		minWidth = msie < 7 ? "width" : "minWidth",
+		selectQueue;
+
+	function create(className, nodeName){
+		return $("<" + ( nodeName || "div" ) + "/>").addClass(className);
+	}
+
+	if( msie < 8 ) {
+		function fixie(selectui, select){
+			if(selectui.find(".select_menu_ui").length){
+				return;
+			}
+			selectui.bind("selectstart", function(e){
+				e.stopImmediatePropagation();
+				return false;
+			}).click(function(e) {
+				var menu = selectui.find(".select_menu_ui");
+				if( menu.position().left < 0 ) {
+					showMenu(selectui, menu, select);
+				} else {
+					hideMenu(selectui, menu);
+				}
+				selectui.addClass("select_focus_ui");
+			});
+			$(select).focus(function(){
+				selectui.addClass("select_focus_ui");
+			}).blur(function(e) {
+				hideAll();
+			});
+
+			createMenu(selectui, select);
+
+		}
+
+		//建立下拉菜单
+		function createMenu(selectui, select){
+			var menu = createOpts(create("select_menu_ui").delegate(".option_ui", "mouseenter", function(e) {
+				$(this).addClass("option_hover_ui").siblings().removeClass("option_hover_ui");
+			}), select).appendTo(selectui).hover(function(){
+				$(this).width(this.clientWidth);
+			},function(){
+				$(this).width("");
+			});
+			return menu;
+		}
+
+		//建立下拉菜单中的选项
+		function createOpts(menu, select){
+			menu.html("");
+			$.each(select.options, function(i){
+				create("option_ui").html(this.innerHTML + "&nbsp;").click(function(e) {
+					select.selectedIndex = i;
+					$(select).trigger("change");
+				}).appendTo(menu);
+			});
+			menu.children().eq(select.selectedIndex).addClass("option_hover_ui");
+			return menu.css("left", "-99999em");
+		}
+
+		//除not外，关闭所有select
+		function hideAll(not){
+			var tag = $(".select_ui");
+			if(not){
+				tag = tag.not(not);
+			}
+			tag.each(function(i) {
+				var selectui = $(this);
+				selectui.removeClass("select_focus_ui");
+				hideMenu(selectui);
+			});
+		}
+
+		//关闭指定的select下拉菜单
+		function hideMenu(selectui, menu){
+			selectui.css({zIndex: ""});
+			( menu || selectui.find(".select_menu_ui") ).css("left", "-99999em");
+		}
+
+		//显示下拉菜单
+		function showMenu(selectui, menu, select){
+			hideAll(selectui);
+			createOpts(menu, select);
+			selectui.css({zIndex: 0xffff});
+			menu.css({left: 0});
+		}
+
+		//页面点击除select_ui外任意位置，关闭下拉菜单
+		$(document).click(function(e){
+			hideAll($(e.target).closest(".select_ui"));
+		});
+
+	}
+
+	function modifyText(select){
+		//select方式变化时，写入当先选择项文本
+		var index = select.selectedIndex,
+			text = index < 0 ? "" : select.options[index].innerHTML,
+			textdiv = $(select).prev(".select_text_ui");
+		if(!textdiv.length){
+			textdiv = create("select_text_ui").insertBefore(select);
+		}
+		text = text || "&nbsp;"
+		if(textdiv.html() != text ){
+			textdiv.html(text);
+		}
+		//计算select宽度
+		var length = 0;
+		$.each(select.options, function(){
+			var text = this.label || this.innerText || this.textContent || this.innerHTML,
+				width = text.match(/[u0000-u00FF]/g);
+			width = text.length - (width ? width.length / 2 : 0) + 0.5;
+			length = Math.max(width, length);
+		});
+		length += "em";
+
+		if( msie < 7 && document.readyState == "complete" ){
+			textdiv.css( minWidth, "" );
+			if(textdiv.css( minWidth) != "auto" ){
+				return;
+			}
+		}
+		if (textdiv.css( minWidth ) != length) {
+			textdiv.css( minWidth, length );
+		}
+	}
+
+	//不支持propertychange事件的浏览器(除IE6、7、8、9外)中使用定时器来刷select文字
+	function startInterval(select){
+		if(selectQueue){
+			selectQueue.push(select);
+		} else {
+			selectQueue = [select];
+			(function(){
+				$.each(selectQueue, function(){
+					//解决bug，火狐下selectIndex会随菜单项滑动而变化
+					if(!(window.netscape && document.activeElement == this)){
+						modifyText(this);
+					}
+				});
+				setTimeout(arguments.callee, 200);
+			})();
+		}
+	}
+
+	$.fn.selectui = function(){
+		return this.each(function(){
+
+			var modifyTextTimer,
+				select = $(this),
+				selectui = select.closest(".select_ui");
+
+			//给select标签加包裹
+			if(!selectui.length){
+				selectui = create("select_ui", "span");
+				selectui.insertAfter(select).append(create("select_arrow")).append(select);
+			}
+
+			//监听可能改变select选中项的事件
+			select.bind("change propertychange DOMAttrModified DOMNodeInserted DOMNodeRemoved keypress", function(e){
+				//利用定时器过滤多次事件触发，短时间内只运行最后一次
+				clearTimeout(modifyTextTimer);
+				var select = this;
+				modifyTextTimer = setTimeout(function(){
+					modifyText(select);
+				}, e.type == "propertychange" ? 200 : 10);
+			}).each(function(){
+				modifyText(this);
+			});
+			if( msie < 8 ) {
+				//IE6、7中模拟select，并非原生
+				fixie(selectui, this);
+			} else {
+				//其他浏览器添加焦点态样式即可
+				select.focus(function(e){
+					selectui.addClass("select_focus_ui");
+				}).blur(function(e) {
+					selectui.removeClass("select_focus_ui");
+				});
+			}
+			
+			//解决初始化样式后，其他js又修改select选项
+			if( "onpropertychange" in select[0] ){
+				//IE6、7、8、9下延迟触发一次propertychange，避免初始化后其他js为select单纯增加Option但未去修改selectIndex，从而未触发propertychange事件
+				select.triggerHandler("propertychange");
+			} else {
+				//高端浏览器性能有富余，每隔200毫秒检查一次
+				startInterval(select[0]);
+			}
+		});
+	};
+
+})(jQuery);

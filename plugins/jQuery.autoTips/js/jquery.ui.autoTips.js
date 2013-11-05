26923c2786c4660cf21c373e15a6e5b85918f505
flftfqwxf <flftfqwxf@qq.com>
2013-11-05 18:25:55 +0800

    autoTips plugins

--- plugins/jQuery.autoTips/js/jquery.ui.autoTips.js
+++ plugins/jQuery.autoTips/js/jquery.ui.autoTips.js
@@ -12,58 +12,66 @@
             }
             (function (opts) {
                 _this.keyup(function (event) {
-                    var key = event.which, txtVal = _this.val(), tipsID = $('#' + opts.tipsID), li_list = $('#' + opts.tipsID).find('li');
-                    if (key == 13) {//回车
-                        if (tipsID.is(':visible')) {
-                            _this.val(li_list.eq(getCurIndex(li_list, key, opts)).html());
-                            tipsID.hide();
-                            return false;
-                            //$('#password').focus();
-                        }
-                    } else if (key == 40 || key == 38) {//上下键输入38为上，40为下
-                        if (tipsID.is(':visible')) {
-                            var cur_item = li_list.eq(getCurIndex(li_list, key, opts));
-                            cur_item.addClass('tips_hover').siblings().removeClass('tips_hover');
-                            _this.val(cur_item.html());
-                        }
-                    } else if (key != 13 && key != 40 && key != 38) {
-                        if ($.trim(txtVal) == "") {
-                            tipsID.hide();
-                            return false;
-                        }
-                        if (opts.type === 'email') {
-                            //根据输入重构提示内容列表
-                            var _dataSource = [];
-                            var prefix = txtVal.indexOf('@') != -1 ? txtVal.split('@')[0] : txtVal;
-                            if (txtVal.indexOf('@') != -1) {
-                                var postfix = txtVal.split('@')[1];
-                                if (postfix != "") {
-                                    for (var u = 0; u < opts.dataSource.length; u++) {
-                                        if (opts.dataSource[u].indexOf(postfix) != -1) {
-                                            _dataSource.push(opts.dataSource[u]);
-                                        }
-                                    }
-                                    _dataSource.unshift(opts.dataSource[0], opts.dataSource[1]);
-                                } else if (postfix == "") {
-                                    _dataSource = opts.dataSource;
-                                } else {
-                                    _dataSource.unshift(opts.dataSource[0], opts.dataSource[1]);
-                                }
-                                _creatTipsElement(_dataSource);
-                            } else {
-                                _creatTipsElement(opts.dataSource);
+                    clearTimeout(_this.searching);
+                    _this.searching = setTimeout(function () {
+                        var key = event.which, txtVal = _this.val(), tipsID = $('#' + opts.tipsID), li_list = $('#' + opts.tipsID).find('li');
+                        if (key == 13) {//回车
+                            if (tipsID.is(':visible')) {
+                                _this.val(li_list.eq(getCurIndex(li_list, key, opts)).html());
+                                tipsID.hide();
+                                return false;
                             }
-                            //根据输入重构提示内容列表 END
-                            li_list = $('#' + opts.tipsID).find('li:not(:first)');
-                            li_list.each(function () {
-                                $(this).html(prefix + $(this).attr('data'));
-                            }).eq(opts.defaultIndex).addClass('tips_hover').html(txtVal);
-                        }//opts.type==='email' EMD
-                        if (opts.callback) {
-                            opts.callback.call(this, opts);
+                        } else if (key == 40 || key == 38) {//上下键输入38为上，40为下
+                            if (tipsID.is(':visible')) {
+                                var cur_item = li_list.eq(getCurIndex(li_list, key, opts));
+                                cur_item.addClass('tips_hover').siblings().removeClass('tips_hover');
+                                _this.val(cur_item.html());
+                            }
+                        } else if (key != 13 && key != 40 && key != 38) {//获取数据
+                            if ($.trim(txtVal) == "") {
+                                tipsID.hide();
+                                return false;
+                            }
+
+                            if (opts.type === 'email') {
+                                var dataSource = $.isFunction(opts.dataSource) ? dataSource.call(this, opts) : opts.dataSource;
+                                //根据输入重构提示内容列表
+                                var _dataSource = [];
+                                var prefix = txtVal.indexOf('@') != -1 ? txtVal.split('@')[0] : txtVal;
+                                if (txtVal.indexOf('@') != -1) {
+                                    var postfix = txtVal.split('@')[1];
+                                    if (postfix != "") {
+                                        for (var u = 0; u < dataSource.length; u++) {
+                                            if (dataSource[u].indexOf(postfix) != -1) {
+                                                _dataSource.push(dataSource[u]);
+                                            }
+                                        }
+                                        _dataSource.unshift(dataSource[0], dataSource[1]);
+                                    } else if (postfix == "") {
+                                        _dataSource = dataSource;
+                                    } else {
+                                        _dataSource.unshift(dataSource[0], dataSource[1]);
+                                    }
+                                    _creatTipsElement(_dataSource, opts);
+                                } else {
+                                    _creatTipsElement(dataSource, opts);
+                                }
+                                //根据输入重构提示内容列表 END
+                                li_list = $('#' + opts.tipsID).find('li');
+                                li_list.each(function () {
+                                    $(this).html(prefix + $(this).attr('data'));
+                                }).eq(opts.defaultIndex).addClass('tips_hover').html(txtVal);
+                            } else if($.isFunction(opts.dataSource)) {
+                                opts.dataSource.call(this,opts);
+
+                            }//opts.type==='email' EMD
+                            if (opts.callback) {
+                                opts.callback.call(this, opts);
+                            }
+                            tipsID.show();
                         }
-                        tipsID.show();
-                    }
+                    }, opts.delay);
+
 
                 });
                 //获取当前选中的提示项
@@ -148,7 +156,9 @@
         addClass: null,
         defaultIndex: 0,
         type: 'email',
-        autoWidth: true
+        autoWidth: true,
+        delay: 200,
+        jsonpCallback:null
     };
 })(jQuery);
 //jQuery.ui.autoTips.js END
